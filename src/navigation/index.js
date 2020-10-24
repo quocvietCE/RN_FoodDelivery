d64604b61d4ba3632b8a4828ede12f7b234a5d29
viet.nguyen.successsoftware <viet.nguyen@successsoftware.global>
2020-10-24 21:39:42 +0700

    first commit

---
+++ src/navigation/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {createAppContainer} from 'react-navigation';
+import {enableScreens} from 'react-native-screens';
+import {createStackNavigator} from 'react-navigation-stack';
+import {Image, Platform} from 'react-native';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+
+import Home from '../screens/home';
+import Order from '../screens/order/OrderScreen';
+
+// Icon.loadFont();
+
+// import theme from '../themes';
+enableScreens();
+const screens = createStackNavigator(
+  {
+    Home,
+    Order: {
+      screen: Order,
+      navigationOptions: {
+        headerShown: false,
+      },
+    },
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      //   headerStyle: {
+      //     height: theme.sizes.base * (Platform.OS === 'ios' ? 5 : 4),
+      //     borderBottomColor: '#F2F2F2',
+      //     elevation: 0, // for android
+      //     backgroundColor: Platform.OS === 'ios' ? '#F2F2F2' : 'transparent',
+      //     borderBottomWidth: 5,
+      //   },
+      //   headerBackImage: () => (
+      //     <Icon name="angle-left" color={theme.colors.secondary} size={30} />
+      //   ),
+      //   headerBackTitleVisible: false,
+      //   headerBackTitle: '',
+      //   headerLeftContainerStyle: {
+      //     alignItems: 'center',
+      //     marginLeft: theme.sizes.base + 10,
+      //   },
+      //   headerRightContainerStyle: {
+      //     alignItems: 'center',
+      //     paddingRight: theme.sizes.base,
+      //     backgroundColor: 'transparent',
+      //   },
+      //   headerTitle: '',
+      //   headerTintColor: 'white',
+      headerShown: false,
+    },
+  },
+);
+
+export default createAppContainer(screens);
